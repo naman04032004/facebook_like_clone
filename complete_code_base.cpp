4f7a08fabf4fb3cfaeeccb28f301858c69b4dd3f
Naman <87256361+naman04032004@users.noreply.github.com>
2024-05-22 18:38:06 +0530

    Create complete_code_base.cpp

---
+++ complete_code_base.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define push_back
+using namespace std;
+int main()
+{
+  cout<<" Hello, Nice to meet you ! "<<endl;
+  cout<<" I am giving you the following options, Please "Choose" from these options "<<endl;
+  cout<<" You cam 
+  cout<<" 
+  string s;
+  getline(cin,s);
+  return 0;
+}
